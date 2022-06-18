782006645f78d1485352b980b1de215ccd55b137
fulvio Takara <fulviojp@gmail.com>
2022-06-18 13:24:47 -0300

    first commit

---
+++ ifoodcount/src/components/app/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Ifoodcounter from '../Ifoodcounter/ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Ifood-Counter</h1>
+      <Ifoodcounter/>
+    </>
+  );
+}
+
+export default App;
